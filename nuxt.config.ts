a5e1b8c7fbd8f7b38ba3006fa748a6ebc8c023ad
Yannick Seeger <yannick.sgr@gmail.com>
2023-07-03 12:09:42 +0200

    feat: seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['nuxt-umami'],
+  extends: ['nuxt-umami', 'nuxt-seo-kit'],
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-adsense'],
   devtools: { enabled: true },
   'google-adsense': {
     id: 'ca-pub-9604089991570546'
+  },
+  runtimeConfig: {
+    public: {
+      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://textlength.info',
+      siteName: 'TextLength Counter',
+      siteDescription: 'Count your text and word length online.',
+    }
   }
 })
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="w-screen h-screen flex items-center bg-base-200 flex-col pt-14">
+    <h1 class="text-2xl font-bold">Text Length Counter</h1>
+    <h2>Count your text length and words.</h2>
+    <div class="container max-w-2xl space-y-2 mt-4 px-4">
+      <div class="flex flex-col gap-1">
+        <label for="textInput" class="font-medium">Enter your text:</label>
+        <textarea v-model="textInput" id="textInput" rows="10"  class="textarea textarea-bordered"></textarea>
+      </div>
+      <p class="font-medium">Length: <span class="font-bold">{{ textInput.length }}</span></p>
+      <p class="font-medium">Words: <span class="font-bold">{{ wordCount }}</span></p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const textInput = ref('');
+
+const wordCountRegex = new RegExp(/\w+/g);
+
+const wordCount = computed(() => {
+  const test = textInput.value.match(wordCountRegex);
+  return test ? test.length : 0;
+});
+</script>
